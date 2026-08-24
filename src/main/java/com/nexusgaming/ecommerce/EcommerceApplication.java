94e08250c12c526d300a5a5038c6692b4cdfa4bb
Lucas Carmona <lucascarmona384@gmail.com>
2026-08-24 16:50:55 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/nexusgaming/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.nexusgaming.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
